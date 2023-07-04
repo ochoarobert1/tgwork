b932991f7b6cc30977db4483faaf8c0db3a568c5
Robert Ochoa <rochoa@modusagency.com>
2023-07-04 13:29:48 -0400

    Register Elementor automatico - por arreglar

--- themes/hello-elementor-child/includes/elementor.php
+++ themes/hello-elementor-child/includes/elementor.php
@@ -17,7 +17,8 @@ class TGElementorWidgets
 	public function __construct()
 	{
 		add_action('elementor/elements/categories_registered', [$this, 'add_elementor_widget_categories']);
-		add_action('wp_enqueue_scripts', [$this, 'elementor_test_widgets_dependencies'], 80);
+		//add_action('wp_enqueue_scripts', [$this, 'elementor_test_widgets_dependencies'], 80);
+		add_action('elementor/widgets/register', [$this, 'register_list_widget']);
 	}
 
 	public function add_elementor_widget_categories($elements_manager)
@@ -31,9 +32,30 @@ class TGElementorWidgets
 		);
 	}
 
-	function elementor_test_widgets_dependencies()
+	function elementor_test_widgets_dependencies($widgets)
 	{
-		$path = get_template_directory_uri() . '/assets/css/elementor';
-		$files = array_diff(scandir($path), array('.', '..'));
+	}
+
+	public function register_list_widget($widgets_manager )
+	{
+		$path = get_stylesheet_directory() . '/widgets';
+		$ffs = scandir($path);
+		unset($ffs[array_search('.', $ffs, true)]);
+		unset($ffs[array_search('..', $ffs, true)]);
+
+		// prevent empty ordered elements
+		if (count($ffs) < 1)
+			return;
+
+		foreach ($ffs as $ff) {
+			$className = preg_replace('/\s+/', '', ucwords(preg_replace('/[-]/', ' ', $ff)));
+			require_once $path . '/' . $ff . '/widget.php';
+
+			var_dump($className);
+
+			$widgets_manager->register(new $className());
+		}
 	}
 }
+
+new TGElementorWidgets;
